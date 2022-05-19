741312b73224870c88328e1ad142dede36ba368f
Paul Yorde <paulyorde26@gmail.com>
2022-05-18 20:43:27 -0400

    app module updates track / player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TracksComponent } from './tone-components/tracks/tracks.component';
+import { PlayerComponent } from './tone-components/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TracksComponent,
+    PlayerComponent,
   ],
   imports: [
     BrowserModule
