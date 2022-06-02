3bd48d1b5777f6a04bd4be4bdaed01b4334a8bc2
Paul Yorde <paulyorde26@gmail.com>
2022-06-02 08:01:15 -0400

    clear visualizer

--- src/app/tone-components/tracks/tracks.component.ts
+++ src/app/tone-components/tracks/tracks.component.ts
@@ -14,6 +14,9 @@ export class TracksComponent implements OnInit {
 
   toneContextSubject = new BehaviorSubject<any>(null)
   toneContext$ = this.toneContextSubject.asObservable()
+
+  audioUrlSubject = new BehaviorSubject<any>(null)
+  audioUrl$ = this.audioUrlSubject.asObservable() 
   
 
   _reverb = new Tone.Reverb({ "wet": 1, "decay": 1.9, "preDelay": 1.00 })
@@ -33,6 +36,7 @@ export class TracksComponent implements OnInit {
   webAudioContext = new AudioContext()
   webAudioContextStateSubject = new BehaviorSubject<any>(null);
   webAudioContext$ = this.webAudioContextStateSubject.asObservable()
+  stop3d: boolean = false;
 
   
 
@@ -66,25 +70,18 @@ export class TracksComponent implements OnInit {
 
 
   async analyze3D(stream?: any) {
+    const self = this
     this.sourceNode = this.webAudioContext.createMediaStreamSource(stream as MediaStream)
     let analyser = this.webAudioContext.createAnalyser()
     await this.sourceNode.connect(analyser)
-
-    //TODO apply this to play(audio) 
-    // analyser.connect(this.webAudioContext.destination);
     analyser.fftSize = 512;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-    console.log('buffer lenght', bufferLength)
-
     const barWidth = 15;
     let barHeight: number;
-
     let canvas = document.getElementById('canvas') as HTMLCanvasElement
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
     let x;
     let ctx: any
     ctx = canvas.getContext('2d')
@@ -92,27 +89,35 @@ export class TracksComponent implements OnInit {
     
 
     function animate() {
+      
       x = 0
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       analyser.getByteFrequencyData(dataArray);
       drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray, ctx, canvas);
-      reqId = requestAnimationFrame(animate)
       
+      reqId = requestAnimationFrame(animate)
+
+      if(self.stop3d) {
+        console.log('3d stop')
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
     }
     animate()
 
     this.webAudioContext$.subscribe(state => {
       if(state=='suspended') {
         console.log('uss state::', state)
+        this.stop3d = true
         cancelAnimationFrame(reqId)
+        
       } else if(state=='running') {
         console.log('running state::', state)
-        // animate()
       }
     })
     
 
     function drawVisualiser(bufferLength: number, x: number, barWidth: number, barHeight: number, dataArray: any, ctx: CanvasRenderingContext2D, canvas: { width: number; height: number; }) {
+    
       for (let i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i] * 1.5;
         ctx.save();
@@ -201,9 +206,12 @@ export class TracksComponent implements OnInit {
     recording.onstop = () => {
       /** Send to Download */
       this.recordingSubject.next(new Blob(data))
+
       new Blob(data).arrayBuffer()
         .then(arrayBuffer => this.webAudioContext.decodeAudioData(arrayBuffer))
         .then(audioBuffer => sourceNode.buffer = audioBuffer)
+
+    // this.audioUrlSubject.next(sourceNode.buffer)  
         
     }
     /** Send to Player */
